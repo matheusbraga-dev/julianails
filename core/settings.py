63d2cb1d49298e15fe9f4466bd7c4721f0e2a1a7
Matheus Kepy <matheuskdev@gmail.com>
2026-01-11 00:58:10 -0300

    fix: Atualiza configurações de arquivos estáticos e de mídia no Django

--- core/settings.py
+++ core/settings.py
@@ -122,10 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "core/templates/static"),)
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATICFILES_DIRS = [
+    BASE_DIR / 'core/templates/static'
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = "/media/"
 
 LOGIN_URL = "login"
